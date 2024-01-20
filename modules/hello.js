b78a038877737619265d4a269f670f96e896a94a
thisissikandar <sikandarc989@gmail.com>
2024-01-20 22:09:12 +0530

    modules require imports

---
+++ modules/hello.js
@@ -0,0 +1,4 @@
+console.log("hello World")
+
+const math = require("./math.js")
+console.log(math.add(5));
---
+++ modules/math.js
@@ -0,0 +1,12 @@
+function add (b){
+    return 5 +b
+}
+function sub (b){
+    return 5 +b
+}
+// This way cannot export becouse its overwrite the intial value
+// module.exports = add
+// module.exports = sub
+
+// for multiple export we can use Object like this 
+module.exports = {add, sub}
